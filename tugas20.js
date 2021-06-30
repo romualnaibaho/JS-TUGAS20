f57bbd5b81a3b5a20a96d00db634b239270efa1d
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 22:57:58 +0700

    js

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+function finds(){
+    var str = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+
+    console.log(str.match("are"));
+    console.log(str.match("words"))
+}
+
+finds();
